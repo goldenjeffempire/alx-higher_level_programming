39357b0e449e4b746589e8852fade05b4fb9a6ec
goldenjeffempire <jeffemuodafe124@gmail.com>
2023-12-07 05:50:27 -0800

    Multiply

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
-
-
-def best_score(a_dictionary):
-    """
-    A function that returns a key with the biggest integer value.
-    """
-    if a_dictionary:
-        my_list = list(a_dictionary.keys())
-        score = 0
-        leader = ""
-        for i in my_list:
-            if a_dictionary[i] > score:
-                score = a_dictionary[i]
-                leader = i
-        return leader
+def multiply_list_map(my_list=[], number=0):
+    return (list(map((lambda i: i * number), my_list)))
