ab54f2463d752812a1b65a3f658f2a46aa994d38
goldenjeffempire <jeffemuodafe124@gmail.com>
2024-01-09 05:26:01 -0800

    Class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,8 +4,4 @@
 
 def class_to_json(obj):
     """Return the dictionary represntation of a simple data structure."""
-    json_dict = {}
-    for attr, value in obj._dict_.items():
-        if isinstance(value,(list,dict, str, int, bool)):
-            json_dict[attr] = value
-    return json_dict
+    return obj.__dict__
