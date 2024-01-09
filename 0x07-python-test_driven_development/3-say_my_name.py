147f025655b2dc2125a30810c5c268c26567ccdb
goldenjeffempire <jeffemuodafe124@gmail.com>
2024-01-09 02:56:36 -0800

    Test driven development

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-"""Defines a name-printing function."""
+"""
+File: 3-say_my_name.py
+
+By: Samson Tedla <samitedla23@gmail.com>
+
+Defines a function that prints "My name is <first name> <last name>"
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Print a name.
+    """
+    Prints "My name is <first name> <last name>"
 
     Args:
-        first_name (str): The first name to print.
-        last_name (str): The last name to print.
+        first_name (string): first name input
+        last_name (string); last name input
     Raises:
-        TypeError: If either of first_name or last_name are not strings.
+        TypeError: if first_name is not a string
+        TypeError: if last_namme is not a string
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
